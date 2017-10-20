f7410c6fd01e96bebcdbc65d471a25a9595aa6b8
Amir <amir.ghandeharioon@gmail.com>
2017-10-20 21:11:46 +0000

    connected to MongoDB

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,7 +24,7 @@ function createTweetElement (tweet) {
   $article.append($tweetContent);
 // Footer part
   let $footer = $("<footer>").addClass("clearfix");
-  let $dayCounter = $("<p>").text(calculateSince(tweet.created_at));
+  let $dayCounter = $("<p>").text(moment(moment(tweet.created_at).format()).fromNow());
   $footer.append($dayCounter);
   let $div = $("<div>").addClass("icons");
   let $heart = $("<i>").addClass("fa fa-heart").attr("aria-hidden", "true");
@@ -107,53 +107,6 @@ $(".new-tweet form").submit(function (event) {
     $(".counter").text(140) // Resets the character counter to 140
     }
 });
-
-
-//=====================================================================================================================================
-// Calculate Since Function
-//=====================================================================================================================================
-
-function calculateSince(datetime) {
-  var tTime = new Date(datetime);
-  var cTime = new Date();
-  var sinceMin = Math.round((cTime - tTime) / 60000);
-  if (sinceMin === 0) {
-      var sinceSec = Math.round((cTime - tTime) / 1000);
-      if (sinceSec < 10)
-        var since = 'less than 10 seconds ago';
-      else if (sinceSec < 20)
-        var since = 'less than 20 seconds ago';
-      else
-        var since = 'half a minute ago';
-  }
-  else if(sinceMin === 1) {
-      var sinceSec = Math.round((cTime - tTime) / 1000);
-      if (sinceSec === 30)
-        var since = 'half a minute ago';
-      else if(sinceSec<60)
-        var since = 'less than a minute ago';
-      else
-        var since = '1 minute ago';
-  }
-  else if (sinceMin < 45)
-      var since = sinceMin + ' minutes ago';
-  else if (sinceMin > 44 && sinceMin < 60)
-      var since = 'about 1 hour ago';
-  else if (sinceMin < 1440) {
-      var sinceHr = Math.round(sinceMin / 60);
-  if (sinceHr === 1)
-    var since = 'about 1 hour ago';
-  else
-    var since = 'about ' + sinceHr + ' hours ago';
-  }
-  else if (sinceMin > 1439 && sinceMin < 2880)
-      var since = '1 day ago';
-  else {
-      var sinceDay = Math.round(sinceMin / 1440);
-      var since = sinceDay + ' days ago';
-  }
-  return since;
-};
 
 
 
